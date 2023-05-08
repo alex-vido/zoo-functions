64b3389bc3becbcad9d6cd959fedf221a1be10df
Alex Vido <alex.vido93@gmail.com>
2023-05-08 17:05:33 -0300

    Feito exercício 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 const locateAnimalName = (id) => data.species.find((animal) =>
   animal.id === id);
+
 const getSpeciesByIds = (...ids) => ids.map((id) => locateAnimalName(id));
 
 module.exports = getSpeciesByIds;
