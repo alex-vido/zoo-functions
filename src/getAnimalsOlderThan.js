aab7e40f66aae8502ed205fecf7e95d7a4c3a36f
Alex Vido <alex.vido93@gmail.com>
2023-05-09 10:28:46 -0300

    Feito exercício 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,18 +1,11 @@
 const data = require('../data/zoo_data');
 
-const isOlder = (element, age) => {
-  element.every((el) => el.every());
-};
+const isOlder = (element, age) => element.residents.every((el) => el.age >= age);
 
-const getAnimalsOlderThan = (animal, age) => data.species.map((element) => isOlder(element, age));
-
-console.log(getAnimalsOlderThan(['lions', 11]));
-
-/*
-Fazer um laço parra cada animal em animal
-comparar se o nome dele consta em data.especies.name
-se conter o nome, localizar a idade dele em data.species.residents.age
-Comparar se ele possui a idade igual ou maior que age.
-*/
-
+const getAnimalsOlderThan = (animal, age) => {
+  const specie = data.species.find((element) => element.name === animal);
+  if (specie) {
+    return isOlder(specie, age);
+  }
+}
 module.exports = getAnimalsOlderThan;
