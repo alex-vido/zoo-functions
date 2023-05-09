eb1f054e9bae8e4845b23531538f3b67d5a42ed8
Alex Vido <alex.vido93@gmail.com>
2023-05-09 10:40:29 -0300

    Exercício 2 refatorado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,7 @@
 const data = require('../data/zoo_data');
 
-const isOlder = (element, age) => element.residents.every((el) => el.age >= age);
+const getAnimalsOlderThan = (animal, age) =>
+  data.species.find((element) => element.name === animal)
+    .residents.every((el) => el.age >= age);
 
-const getAnimalsOlderThan = (animal, age) => {
-  const specie = data.species.find((element) => element.name === animal);
-  if (specie) {
-    return isOlder(specie, age);
-  }
-};
 module.exports = getAnimalsOlderThan;
