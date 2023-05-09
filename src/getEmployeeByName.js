e9ded2f1e4a1de2adff2bc9d975ca63f987fda19
Alex Vido <alex.vido93@gmail.com>
2023-05-09 11:03:25 -0300

    Corrigido exercício 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,6 +3,6 @@ const data = require('../data/zoo_data');
 const getEmployeeByName = (employeeName) =>
   data.employees.find((person) => employeeName === person.firstName
     || employeeName === person.lastName)
-      || [];
+      || {};
 
 module.exports = getEmployeeByName;
