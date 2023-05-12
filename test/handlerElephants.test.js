cd75873df6df58d35ce1a9cbfbb951ff39790690
Alex Vido <alex.vido93@gmail.com>
2023-05-12 17:55:58 -0300

    Feito exercício 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,14 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se a função retorna o esperado', () => {
+    expect(typeof handlerElephants).toBe('function');
+    expect(handlerElephants()).toBeUndefined();
+    expect(handlerElephants(2)).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants('location', 'elephants')).toBe('NW');
+    expect(handlerElephants('asd', 'elephants')).toBeNull();
+    expect(handlerElephants('names', 'elephants')).toEqual([ 'Ilana', 'Orval', 'Bea', 'Jefferson' ]);
+    expect(handlerElephants('averageAge', 'elephants')).toBe(10.5);
+    expect(handlerElephants('count', 'elephants')).toBe(4);
+  });
+});
