eaf568b866cb2728b3707db2cf7ea85b7864d877
Alex Vido <alex.vido93@gmail.com>
2023-05-15 15:47:00 -0300

    Corrigido exercício 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,30 @@ const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
   // seu código aqui
+  const ent = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      ent.child += 1;
+    } else if (entrant.age < 50) {
+      ent.adult += 1;
+    } else {
+      ent.senior += 1;
+    }
+  });
+  return ent;
 };
 
 const calculateEntry = (entrants) => {
   // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  const ent = countEntrants(entrants);
+  let sum = 0;
+  sum += ent.child * data.prices.child;
+  sum += ent.adult * data.prices.adult;
+  sum += ent.senior * data.prices.senior;
+  return sum;
 };
 
 module.exports = { calculateEntry, countEntrants };
