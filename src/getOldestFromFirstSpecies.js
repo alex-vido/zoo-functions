5406a3ae1eee97fbf79cf36eaf77ae6a1c64066e
Alex Vido <alex.vido93@gmail.com>
2023-05-15 16:44:40 -0300

    Feito exercício 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const getOldestAnimal = (idAnimal) =>
+  Object.values(data.species.find((animal) => animal.id === idAnimal)
+    .residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr)));
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const idAnimal = data.employees.find((emp) => emp.id === id)
+    .responsibleFor[0];
+  return getOldestAnimal(idAnimal);
 };
 
 module.exports = getOldestFromFirstSpecies;
