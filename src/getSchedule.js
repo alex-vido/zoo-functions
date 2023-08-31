02c457b443bb805dfc6148fd3a36c64ff7f75e60
Alex Vido <alex.vido93@gmail.com>
2023-08-31 16:32:42 -0300

    Pubishing Project

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
-};
+const animalsDay = (day) =>
+  data.species.filter((animal) => animal.availability.includes(day))
+    .map((a) => a.name);
+
+const allSchedule = () =>
+  Object.entries(data.hours).reduce((schedule, [day, { open, close }]) => {
+    const retorno = schedule;
+    if (open > 0) {
+      retorno[day] = {
+        officeHour: `Open from ${open}am until ${close}pm`,
+        exhibition: animalsDay(day),
+      };
+    } else {
+      retorno[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!' };
+    }
+    return retorno;
+  }, {});
+
+const getSchedule = (scheduleTarget) => (scheduleTarget ? data.species.find((specie) =>
+  specie.name === scheduleTarget).availability : allSchedule());
 
 module.exports = getSchedule;
